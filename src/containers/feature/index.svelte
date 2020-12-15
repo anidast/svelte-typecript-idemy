4269eb3704d1c7b7aa1a03fabc46d3766d4f639d
anidast <anidwi.ad1@gmail.com>
2020-12-15 15:41:27 +0700

    feature

--- src/containers/feature/index.svelte
+++ src/containers/feature/index.svelte
@@ -13,12 +13,15 @@
 </script>
 
 <section>
-	<div class="container">
+	<div class="container my-4">
 		<div class="has-text-centered py-6">
 			<p class="title is-3">How we help our students</p>
-			<p class="subtitle is-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>	
+			<p class="subtitle is-6">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+				sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			</p>	
 		</div>
-		<div class="columns is-multiline is-centered px-6">
+		<div class="columns is-multiline px-6">
 			{#each featureList as feature}
 			<div class="column is-half has-text-centered p-6">
 				<center>
@@ -30,6 +33,22 @@
 				<p class="subtitle is-6">{feature.subtitle}</p>
 			</div>
 			{/each}
+		</div>
+	</div>
+	<div class="columns is-vcentered my-6">
+		<div class="column has-text-centered">
+			<figure class="image is-16by9">
+				<img src="edusys.jpg" alt="Advance Education System">
+			</figure>
+		</div>
+		<div class="column is-two-fifths px-6">
+			<p class="title is-3 my-6">Advanced Education System</p>
+			<p class="subtitle is-6">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+				sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			</p>	
 		</div>
 	</div>
 </section>
