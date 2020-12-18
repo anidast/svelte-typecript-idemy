96108d3293073498dfd44112ca9cb4f84a61af76
anidast <anidwi.ad1@gmail.com>
2020-12-18 15:08:39 +0700

    hero style

--- src/containers/hero/index.svelte
+++ src/containers/hero/index.svelte
@@ -6,13 +6,13 @@ import { Hero } from "..";
 <section class="hero is-fullheight"style="background-image: url({"hero.jpg"});">
 	<div class="hero-body">
 	  <div class="container has-text-centered">
-		<h1 class="title is-family-primary has-text-weight-bold has-text-black">
+		<h1 class="title is-size-2 is-family-primary has-text-weight-bold has-text-black">
 			Choose the Right Course for Learning
 		</h1>
-		<h2 class="subtitle has-text-black">
+		<h2 class="subtitle has-text-black has-text-weight-semibold">
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 		</h2>
-		<a class="button is-rounded is-link" href="/">START NOW</a>
+		<a class="button is-rounded is-link has-text-weight-bold" href="/">START NOW</a>
 	  </div>
 	</div>
 </section>
